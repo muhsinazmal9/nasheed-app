0637a2fc8cf35d0be39525b5e8b44668dd2dab2f
Muhsin Azmal <muhsinstudent12@gmail.com>
2024-08-14 23:29:33 +0600

    api prefix get from boot service provider

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
-Route::get('v1/categories', [\App\Http\Controllers\Api\v1\CategoryController::class, 'index']);
+Route::get('categories', [\App\Http\Controllers\Api\v1\CategoryController::class, 'index']);
+
