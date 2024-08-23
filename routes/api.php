8c9ef8056b3e53d964033924f3df4bbdf5ae87b3
Muhsin Azmal <muhsinstudent12@gmail.com>
2024-08-23 17:33:13 +0600

    remove unnecessary codes

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 // Public APIs
 Route::get('/dedications', [DedicationController::class, 'index']);
