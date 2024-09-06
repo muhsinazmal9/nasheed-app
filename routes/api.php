8e2e4267df20d41f3d215e75a3a00c2fd97adb2c
Muhsin Azmal <muhsinstudent12@gmail.com>
2024-09-06 12:57:53 +0600

    api routing modify

--- routes/api.php
+++ routes/api.php
@@ -6,24 +6,30 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\DedicationController;
 
-// Authentication related APIs (Public)
+// Authentication related APIs
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-// Authentication related APIs (Private)
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', [AuthController::class, 'getAuthenticatedUser']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
 
 /**
  * Public APIs
  */
 
-// dedications
-Route::get('/dedications', [DedicationController::class, 'index']);
-Route::get('/dedications/{dedication}', [DedicationController::class, 'show']);
+Route::controller(DedicationController::class)->group(function () {
+    Route::get('/dedications', 'index');
+    Route::get('/dedications/{dedication}', 'show');
+});
 
-// artists
-Route::get('/artists', [ArtistController::class, 'index']);
-Route::get('/artists/{artist}', [ArtistController::class, 'show']);
+
+Route::controller(ArtistController::class)->group(function () {
+    Route::get('/artists', 'index');
+    Route::get('/artists/{artist}', 'show');
+});
+
+/**
+ * Private APIs
+ */
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'getAuthenticatedUser']);
+});
