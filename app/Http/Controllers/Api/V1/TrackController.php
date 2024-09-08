c6b85d9f7c4183f37f6460733b1d753ef7d511e4
Muhsin Azmal <muhsinstudent12@gmail.com>
2024-09-09 00:11:17 +0600

    query filters add in tracks

--- app/Http/Controllers/Api/V1/TrackController.php
+++ app/Http/Controllers/Api/V1/TrackController.php
@@ -12,9 +12,23 @@ class TrackController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $tracks = Track::active()->get();
+        $tracks = Track::query()->active();
+
+        if ($request->has('artist_id')) {
+            $tracks = $tracks->whereHas('artists', fn ($q) => $q->where('id', $request->artist_id));
+        }
+
+        if ($request->has('lyricist_id')) {
+            $tracks = $tracks->where('lyricist_id', $request->lyricist_id);
+        }
+
+        if ($request->has('dedication_id')) {
+            $tracks = $tracks->where('dedication_id', $request->dedication_id);
+        }
+
+        $tracks = $tracks->get();
 
         return success('Tracks retrieved successfully', TrackResource::collection($tracks));
     }
